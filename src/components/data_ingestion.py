80eec0816b151faa771b6ba39971268361e631fb
sivacharan2001 <sivacharany2001@gmail.com>
2024-07-11 21:33:41 +0530

    comments

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 
+#creating file paths to store the data
 
 @dataclass
 class DataIngestionConfig:
@@ -32,6 +33,7 @@ class DataIngestion:
                logging.info('Train test split initiated')
                train_set,test_set=train_test_split(df,test_size=0.2,random_state=0)
 
+                #sending train_set and test_set into the path specified
                train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
                test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
